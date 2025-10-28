4801b6d16091c4af88153a663623ea5a6c600240
subhayan-newcoder <mail4subhayan@gmail.com>
2025-10-28 06:56:46 +0000

    new changes
    Signed-off-by: subhayan-newcoder <mail4subhayan@gmail.com>

--- backend.tf
+++ backend.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket      = "tf-state-datavision"
-    prefix      = "terraform/state/prod"
-    credentials = "gcp-key.json"
+    bucket = "tf-state-datavision"
+    prefix = "terraform/state/prod"
   }
 }
